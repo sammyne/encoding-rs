37031aae278ce4252af3eb88f7f73d92f737ef7b
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-04-07 13:09:49 +0000

    doc(binary): document public APIs

--- src/binary/mod.rs
+++ src/binary/mod.rs
@@ -1,3 +1,20 @@
+//! Module binary implements simple translation between numbers and byte
+//! sequences and encoding and decoding of varints.
+//!
+//! Numbers are translated by reading and writing fixed-size values.
+//! A fixed-size value is either a fixed-size arithmetic
+//! type (bool, i8, u8, i16, f32, ...)
+//! or an array or struct containing only fixed-size values.
+//!
+//! The varint functions encode and decode single integer values using
+//! a variable-length encoding; smaller values require fewer bytes.
+//! For a specification, see
+//! <https://developers.google.com/protocol-buffers/docs/encoding>.
+//!
+//! This module favors simplicity over efficiency. Clients that require
+//! high-performance serialization, especially for large data structures,
+//! should look at more advanced solutions such as protocol buffers.
+
 mod binary;
 mod varint;
 
