9d0b4720d1129528532bf5b4f8203c7e3e21cfee
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-06-09 05:34:08 +0000

    refactor: package hex APIs as independent crate

---
+++ crates/hex/src/lib.rs
@@ -0,0 +1,10 @@
+//! Implementation of hexadecimal encoding and decoding.
+//!
+
+mod dumper;
+mod errors;
+mod hex;
+
+pub use crate::hex::*;
+pub use dumper::*;
+pub use errors::*;
--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ pub mod ascii85;
 pub mod base32;
 pub mod base64;
 pub mod binary;
-pub mod hex;
 
 pub use errors::*;
 
 pub use csv;
+pub use hex;
