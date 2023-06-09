aa98f64c4d531cdfd3bf26434366f538e84775c1
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-06-09 05:39:25 +0000

    feat(hex): impl std::error::Error for Error

--- crates/hex/src/errors.rs
+++ crates/hex/src/errors.rs
@@ -1,8 +1,11 @@
-#[derive(Debug)]
+/// Errors about decoding/encoding.
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
     /// ErrLength reports an attempt to decode an odd-length input
     /// using [decode][crate::hex::decode] or [decode_string][crate::hex::decode_string].
+    #[error("odd length of hex string")]
     ErrLength(u32),
     /// InvalidByteError values describe errors resulting from an invalid byte in a hex string.
+    #[error("invalid byte: {}", *.1 as char)]
     InvalidByteError(usize, u8),
 }
