fb800bca11294b9adbccaad2442bf31fe9961da2
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-06-10 08:46:46 +0000

    chore(hex): tidy up tests

--- crates/hex/tests/hex.rs
+++ crates/hex/tests/hex.rs
@@ -4,119 +4,10 @@ use lazy_static::lazy_static;
 
 use hex::{self, Error};
 
-const EXPECTED_HEX_DUMP: &'static str = r##"00000000  1e 1f 20 21 22 23 24 25  26 27 28 29 2a 2b 2c 2d  |.. !"#$%&'()*+,-|
-00000010  2e 2f 30 31 32 33 34 35  36 37 38 39 3a 3b 3c 3d  |./0123456789:;<=|
-00000020  3e 3f 40 41 42 43 44 45                           |>?@ABCDE|
-"##;
-
-fn encode_decode_test_cases() -> Vec<(String, Vec<u8>)> {
-    vec![
-        (String::from(""), vec![]),
-        (
-            String::from("0001020304050607"),
-            vec![0, 1, 2, 3, 4, 5, 6, 7],
-        ),
-        (
-            String::from("08090a0b0c0d0e0f"),
-            vec![8, 9, 10, 11, 12, 13, 14, 15],
-        ),
-        (
-            String::from("f0f1f2f3f4f5f6f7"),
-            vec![0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7],
-        ),
-        (
-            String::from("f8f9fafbfcfdfeff"),
-            vec![0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff],
-        ),
-        (String::from("67"), vec![b'g']),
-        (String::from("e3a1"), vec![0xe3, 0xa1]),
-    ]
-}
-
-#[test]
-fn dumper() {
-    let data = {
-        let mut v = [0u8; 40];
-        for i in 0..v.len() {
-            v[i] = (i + 30) as u8;
-        }
-        v
-    };
-
-    for stride in 1..data.len() {
-        let mut out = Vec::new();
-        let mut dumper = hex::dumper(&mut out);
-
-        let mut done = 0usize;
-
-        while done < data.len() {
-            let mut todo = done + stride;
-            if todo > data.len() {
-                todo = data.len();
-            }
-
-            let _ = dumper.write(&data[done..todo]);
-            done = todo;
-        }
-
-        let _ = dumper.flush();
-        std::mem::drop(dumper);
-
-        let got = String::from_utf8(out).expect("nonn-utf8 string");
-        assert_eq!(EXPECTED_HEX_DUMP, &got);
-    }
-}
-
-#[test]
-fn dump() {
-    let data = {
-        let mut v = [0u8; 40];
-        for i in 0..v.len() {
-            v[i] = (i + 30) as u8;
-        }
-        v
-    };
-
-    let got = hex::dump(&data[..]);
-
-    assert_eq!(EXPECTED_HEX_DUMP, got);
-}
-
-#[test]
-fn dump_doubleclose() {
-    let mut out = Vec::new();
-    let mut dumper = hex::dumper(&mut out);
-
-    let _ = dumper.write(b"gopher");
-    let _ = dumper.flush();
-    let _ = dumper.flush();
-
-    let _ = dumper.write(b"gopher");
-    let _ = dumper.flush();
-    std::mem::drop(dumper);
-
-    let expected = "00000000  67 6f 70 68 65 72                                 |gopher|\n";
-    let got = String::from_utf8(out).expect("invalid utf8 string");
-    assert_eq!(expected, got);
-}
-
-#[test]
-fn dump_earlyclose() {
-    let mut out = Vec::new();
-    let mut dumper = hex::dumper(&mut out);
-
-    let _ = dumper.flush();
-    let _ = dumper.write(b"gopher");
-    std::mem::drop(dumper);
-
-    assert_eq!(out.len(), 0, "should write no data");
-}
-
 #[test]
 fn decode_string() {
-    let cases = encode_decode_test_cases();
-    for v in cases.iter() {
-        let (src, expect) = v;
+    for v in ENC_DEC_TESTS.iter() {
+        let (src, expect) = (v.enc, v.dec);
 
         let got = hex::decode_string(src).unwrap();
         assert_eq!(expect, &got);
@@ -181,10 +72,88 @@ fn decoder_err() {
 }
 
 #[test]
+fn dump() {
+    let data = {
+        let mut v = [0u8; 40];
+        for i in 0..v.len() {
+            v[i] = (i + 30) as u8;
+        }
+        v
+    };
+
+    let got = hex::dump(&data[..]);
+
+    assert_eq!(EXPECTED_HEX_DUMP, got);
+}
+
+#[test]
+fn dump_doubleclose() {
+    let mut out = Vec::new();
+    let mut dumper = hex::dumper(&mut out);
+
+    let _ = dumper.write(b"gopher");
+    let _ = dumper.flush();
+    let _ = dumper.flush();
+
+    let _ = dumper.write(b"gopher");
+    let _ = dumper.flush();
+    std::mem::drop(dumper);
+
+    let expected = "00000000  67 6f 70 68 65 72                                 |gopher|\n";
+    let got = String::from_utf8(out).expect("invalid utf8 string");
+    assert_eq!(expected, got);
+}
+
+#[test]
+fn dump_earlyclose() {
+    let mut out = Vec::new();
+    let mut dumper = hex::dumper(&mut out);
+
+    let _ = dumper.flush();
+    let _ = dumper.write(b"gopher");
+    std::mem::drop(dumper);
+
+    assert_eq!(out.len(), 0, "should write no data");
+}
+
+#[test]
+fn dumper() {
+    let data = {
+        let mut v = [0u8; 40];
+        for i in 0..v.len() {
+            v[i] = (i + 30) as u8;
+        }
+        v
+    };
+
+    for stride in 1..data.len() {
+        let mut out = Vec::new();
+        let mut dumper = hex::dumper(&mut out);
+
+        let mut done = 0usize;
+
+        while done < data.len() {
+            let mut todo = done + stride;
+            if todo > data.len() {
+                todo = data.len();
+            }
+
+            let _ = dumper.write(&data[done..todo]);
+            done = todo;
+        }
+
+        let _ = dumper.flush();
+        std::mem::drop(dumper);
+
+        let got = String::from_utf8(out).expect("nonn-utf8 string");
+        assert_eq!(EXPECTED_HEX_DUMP, &got);
+    }
+}
+
+#[test]
 fn encode_to_string() {
-    let cases = encode_decode_test_cases();
-    for v in cases.iter() {
-        let (expect, src) = v;
+    for v in ENC_DEC_TESTS.iter() {
+        let (expect, src) = (v.enc, v.dec);
 
         let got = hex::encode_to_string(src);
         assert_eq!(expect, &got);
@@ -236,28 +205,10 @@ fn encoder_decoder() {
     }
 }
 
-struct EncDecTest {
-    enc: &'static str,
-    dec: &'static [u8],
-}
-
-struct ErrTest {
-    input: &'static str,
-    output: &'static [u8],
-    err: Option<Error>,
-}
-
-impl EncDecTest {
-    fn new(enc: &'static str, dec: &'static [u8]) -> Self {
-        Self { enc, dec }
-    }
-}
-
-impl ErrTest {
-    fn new(input: &'static str, output: &'static [u8], err: Option<Error>) -> Self {
-        Self { input, output, err }
-    }
-}
+const EXPECTED_HEX_DUMP: &'static str = r##"00000000  1e 1f 20 21 22 23 24 25  26 27 28 29 2a 2b 2c 2d  |.. !"#$%&'()*+,-|
+00000010  2e 2f 30 31 32 33 34 35  36 37 38 39 3a 3b 3c 3d  |./0123456789:;<=|
+00000020  3e 3f 40 41 42 43 44 45                           |>?@ABCDE|
+"##;
 
 lazy_static! {
     static ref ENC_DEC_TESTS: Vec<EncDecTest> = vec![
@@ -286,4 +237,27 @@ lazy_static! {
         ErrTest::new("0\x01", b"", Some(Error::InvalidByte(b'\x01'))),
         ErrTest::new("ffeed", b"\xff\xee", Some(Error::Length)),
     ];
+}
+
+struct EncDecTest {
+    enc: &'static str,
+    dec: &'static [u8],
+}
+
+struct ErrTest {
+    input: &'static str,
+    output: &'static [u8],
+    err: Option<Error>,
+}
+
+impl EncDecTest {
+    fn new(enc: &'static str, dec: &'static [u8]) -> Self {
+        Self { enc, dec }
+    }
+}
+
+impl ErrTest {
+    fn new(input: &'static str, output: &'static [u8], err: Option<Error>) -> Self {
+        Self { input, output, err }
+    }
 }
