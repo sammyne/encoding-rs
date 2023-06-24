336270fd5b99809926a5c495d1aa967b900d2fb6
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-06-24 08:47:18 +0000

    doc(pem): document public APIs

--- crates/pem/src/errors.rs
+++ crates/pem/src/errors.rs
@@ -1,9 +1,12 @@
 use std::io;
 
+/// Possible errors during encoding and decoding.
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    /// header key contains colons.
     #[error("cannot encode a header key that contains a colon")]
     BadHeaderKey,
+    /// IO error reported by wrapped writer.
     #[error("io: {0}")]
     Io(io::Error),
 }
